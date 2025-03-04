4f99257f06530dfc21a742f8252daaf520b39209
mgonzalez96 <marianaacevedo1996@gmail.com>
2025-03-03 21:45:57 -0500

    Primer commit

---
+++ src/main/java/com/parcial/api/ApiRestFulApplication.java
@@ -0,0 +1,13 @@
+package com.parcial.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestFulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestFulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parcial/api/ApiRestFulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcial.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestFulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

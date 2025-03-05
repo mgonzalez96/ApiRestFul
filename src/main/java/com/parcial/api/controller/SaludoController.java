c8e17b36fdf6b23d19257c6902b74033d1c3043b
mgonzalez96 <marianaacevedo1996@gmail.com>
2025-03-04 23:26:56 -0500

    Parcial 3 lenguaje 1. Crud BD Normal 2. Crud BD Reactivo 3. List Array 4. Pedido Languaje

--- src/main/java/com/parcial/api/controller/SaludoController.java
+++ src/main/java/com/parcial/api/controller/SaludoController.java
@@ -1,41 +1,28 @@
 package com.parcial.api.controller;
 
 import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.parcial.api.dto.Producto;
 import com.parcial.api.service.ProductoService;
-
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/saludar")
 public class SaludoController {
 
 	@Autowired
 	private MessageSource messageSource;
-	
+
 	@Autowired
 	ProductoService productoService;
-	
 
 	@GetMapping("/saludo")
 	public Mono<String> obtenerSaludo(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
 		return Mono.just(messageSource.getMessage("saludo", null, locale));
-	}
-	
-	
-
-	@GetMapping("/listarProductos")
-	public Flux<Producto> listarProductos() {
-		return productoService.obtenerProductos();
 	}
 
 }
---
+++ src/main/java/com/parcial/api/dto/Pedido.java
@@ -0,0 +1,15 @@
+package com.parcial.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pedido {
+	
+	private String mensaje;
+	private Integer producto;
+
+}
